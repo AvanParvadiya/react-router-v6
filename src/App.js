708986dbaf417bf96c4da1be77979dfbb6d58d4c
unknown <avanparvadiya737@gmail.com>
2021-11-19 14:14:20 +0530

    Add

--- src/App.js
+++ src/App.js
@@ -3,7 +3,53 @@ import MainHeader from "./components/MainHeader";
 import ProductDetail from "./components/ProductDetail";
 import Products from "./components/Products";
 import Welcome from "./components/Welcome";
+// import SelectSearch from "react-select-search";
+import React, { useState } from "react";
+import "./components/select-search.css"
+import SelectSearch, { fuzzySearch } from "react-select-search";
+
+import "@reach/combobox/styles.css";
 function App() {
+  const [value, setValue] = useState("");
+  const [multipleValues, setMultipleValues] = useState([]);
+  const options = [
+    {
+      name: "Annie Cruz",
+      value: "annie.cruz",
+    },
+    {
+      name: "Eli Shelton",
+      value: "eli.shelton",
+    },
+    {
+      name: "Loretta Rogers",
+      value: "loretta.rogers",
+    },
+    {
+      name: "Lloyd Fisher",
+      value: "lloyd.fisher",
+    },
+    {
+      name: "Tiffany Gonzales",
+      value: "tiffany.gonzales",
+    },
+    {
+      name: "Eli Shelton",
+      value: "eli.shelton",
+    },
+    {
+      name: "Loretta Rogers",
+      value: "loretta.rogers",
+    },
+    {
+      name: "Lloyd Fisher",
+      value: "lloyd.fisher",
+    },
+    {
+      name: "Tiffany Gonzales",
+      value: "tiffany.gonzales",
+    },
+  ];
   return (
     <div>
       <MainHeader />
@@ -17,6 +63,22 @@ function App() {
         {/* for sub routing */}
         <Route path="/welcome" element={<Welcome />}></Route>
       </Routes>
+      <React.Fragment>
+        <div style={{ margin: "0 auto", width: 300, marginTop: 100,maxHeight:"20px" }}>
+          <SelectSearch 
+            
+            multiple={false}
+            printOptions={"on-focus"}
+            closeOnSelect={true}
+            options={options}
+            value={multipleValues}
+            onChange={setMultipleValues}
+            search
+            filterOptions={fuzzySearch}
+            placeholder="Search something"
+          />
+        </div>
+      </React.Fragment>
     </div>
   );
 }
